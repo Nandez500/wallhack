dd907be9ca4dc8af56175359afefe12201d4baf0
Brandon Hernandez <brandon.hernandez500@gmail.com>
2018-04-13 15:00:08 -0500

    Removed sendMessageActivity for update
    Removed sendMessageActivity on preparation for updating this branch to integrate with the main branch

--- Android/app/src/main/java/utdallas/wallhack/MainActivity.java
+++ Android/app/src/main/java/utdallas/wallhack/MainActivity.java
@@ -14,20 +14,11 @@ import static android.support.v4.app.ActivityCompat.startActivityForResult;
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "utdallas.wallhack.MESSAGE";
     private ArrayList<BluetoothDevice> bluetoothDevices;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 
     public void startBluetooth (View view) {
