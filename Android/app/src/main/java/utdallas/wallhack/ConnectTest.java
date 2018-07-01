60c170f752d3616afb3b5192649b0c1fdf84ce6a
Brandon Hernandez <brandon.hernandez500@gmail.com>
2018-07-01 14:55:40 -0500

    fixed merge conflicts

--- Android/app/src/main/java/utdallas/wallhack/ConnectTest.java
+++ Android/app/src/main/java/utdallas/wallhack/ConnectTest.java
@@ -1,5 +1,6 @@
 package utdallas.wallhack;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
@@ -9,11 +10,17 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
+
+import static android.content.ContentValues.TAG;
 
 public class ConnectTest extends Activity {
     TextView out;
@@ -21,6 +28,8 @@ public class ConnectTest extends Activity {
     private BluetoothAdapter btAdapter = null;
     private BluetoothSocket btSocket = null;
     private OutputStream outStream = null;
+    private InputStream inStream = null;
+    private CommunicationThread commThread;
 
     // Well known SPP UUID
     private static final UUID MY_UUID = UUID.fromString("88e2a5aa-1fb9-474a-a915-87457661899f");
@@ -59,53 +68,9 @@ public class ConnectTest extends Activity {
         // Set up a pointer to the remote node using it's address.
         BluetoothDevice device = btAdapter.getRemoteDevice(address);
 
-        // Two things are needed to make a connection:
-        //  A MAC address, which we got above.
-        //  A Service ID or UUID.  In this case we are using the
-        //   UUID for SPP.
-        try {
-            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-        } catch (IOException e) {
-            AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
-        }
+        commThread = new CommunicationThread(device);
 
-        // Discovery is resource intensive.  Make sure it isn't going on
-        // when you attempt to connect and pass your message.
-        btAdapter.cancelDiscovery();
-
-        // Establish the connection.  This will block until it connects.
-        try {
-            btSocket.connect();
-            out.append("\n...Connection established and data link opened...");
-        } catch (IOException e) {
-            try {
-                btSocket.close();
-            } catch (IOException e2) {
-                AlertBox("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
-            }
-        }
-
-        // Create a data stream so we can talk to server.
-        out.append("\n...Sending message to server...");
-
-        try {
-            outStream = btSocket.getOutputStream();
-        } catch (IOException e) {
-            AlertBox("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
-        }
-
-        String message = "Hello from Android.\n";
-        byte[] msgBuffer = message.getBytes();
-        try {
-            outStream.write(msgBuffer);
-        } catch (IOException e) {
-            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
-            if (address.equals("00:00:00:00:00:00"))
-                msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
-            msg = msg + ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
-
-            AlertBox("Fatal Error", msg);
-        }
+        commThread.start();
     }
 
     @Override
@@ -114,25 +79,25 @@ public class ConnectTest extends Activity {
 
         out.append("\n...In onPause()...");
 
-        if (outStream != null) {
+        /*if (outStream != null) {
             try {
                 outStream.flush();
             } catch (IOException e) {
                 AlertBox("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
             }
         }
-
         try {
             btSocket.close();
         } catch (IOException e2) {
             AlertBox("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
-        }
+        }*/
     }
 
     @Override
     public void onStop() {
         super.onStop();
         out.append("\n...In onStop()...");
+        commThread.cancel();
     }
 
     @Override
@@ -146,7 +111,7 @@ public class ConnectTest extends Activity {
 
         // Emulator doesn't support Bluetooth and will return null
         if (btAdapter == null) {
-            AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
+            AlertBox("Fatal Error", "Bluetooth not supported. Aborting.");
         } else {
             if (btAdapter.isEnabled()) {
                 out.append("\n...Bluetooth is enabled...");
@@ -159,6 +124,7 @@ public class ConnectTest extends Activity {
     }
 
     public void AlertBox(String title, String message) {
+        Log.e(TAG, message);
         new AlertDialog.Builder(this)
                 .setTitle(title)
                 .setMessage(message + " Press OK to exit.")
@@ -167,5 +133,98 @@ public class ConnectTest extends Activity {
                         finish();
                     }
                 }).show();
+    }
+
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    //out.append(readMessage);
+                    String[] splitMessage = readMessage.split(",");
+                    WallData target = new WallData(
+                            splitMessage[0], Double.valueOf(splitMessage[1]),
+                            Double.valueOf(splitMessage[2]), Double.valueOf(splitMessage[3]),
+                            Double.valueOf(splitMessage[4]));
+                    out.append(target.toString());
+                    Log.i(TAG, "Message Received");
+                    break;
+            }
+        }
+    };
+
+    private class CommunicationThread extends Thread {
+        private final BluetoothSocket btSocket;
+        private final BluetoothDevice btDevice;
+        private InputStream inStream;
+        private OutputStream outStream;
+        private boolean isConnected = false;
+
+        public CommunicationThread(BluetoothDevice device) {
+            btDevice = device;
+            BluetoothSocket tmp = null;
+            try {
+                tmp = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                AlertBox("Fatal Error", "In CommunicationThread and failed to create a Bluetooth Socket" + e.getMessage() + ".");
+            }
+            btSocket = tmp;
+        }
+
+        public void run() {
+            setName("CommunicationThread");
+            Log.i(TAG, "Connecting");
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            btAdapter.cancelDiscovery();
+
+            try {
+                btSocket.connect();
+            } catch (IOException e) {
+                try {
+                    btSocket.close();
+                } catch (IOException e2) {
+                    AlertBox("Fatal Error", "unable to close() socket during connection failure" + e2.getMessage() + ".");
+                }
+                AlertBox("Fatal Error", "Connection Failed" + e.getMessage() + ".");
+            }
+
+            try {
+                tmpIn = btSocket.getInputStream();
+                tmpOut = btSocket.getOutputStream();
+            } catch (IOException e) {
+                AlertBox("Fatal Error", "Socket In/Out Error" + e.getMessage() + ".");
+            }
+
+            inStream = tmpIn;
+            outStream = tmpOut;
+            isConnected = true;
+            Log.i(TAG, "Connected");
+
+            while(isConnected) {
+                try {
+                    bytes = inStream.read(buffer);
+                    mHandler.obtainMessage(0, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    Log.e(TAG, "Connection Lost");
+                    isConnected = false;
+                }
+            }
+        }
+
+        public void cancel() {
+            isConnected = false;
+            try {
+                btSocket.close();
+            } catch (IOException e) {
+                AlertBox("Fatal Error", "close() of socket failed" + e.getMessage() + ".");
+            }
+        }
     }
 }
